2c90c28dc22d663158ecf84f2206431454be9580
iceyy <tombrown4973@gmail.com>
2023-08-26 01:46:47 +0100

    F

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -8000,7 +8000,7 @@ var CakePrice = function (_a) {
     ? React.createElement(
         PriceLink,
         {
-          href: "https://dexscreener.com/base",
+          href: "https://dexscreener.com/shibarium/0xbedd8e7680f3a5bcf2493b06fb20f4a1fcd3b24d",
           target: "_blank",
         },
         React.createElement(Icon$V, { width: "24px", mr: "8px" }),
